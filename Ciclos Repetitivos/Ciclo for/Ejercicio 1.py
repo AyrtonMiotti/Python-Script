17fbd444b7a89dc56cc82a9bf96fc133031caf9d
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-09-29 21:43:28 -0300

    Ejercicio 1

---
+++ Ciclos Repetitivos/Ciclo for/Ejercicio 1.py
@@ -0,0 +1,10 @@
+#Ejercicio 1: Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el mayor de ellos.
+def max_de_tres (n1, n2, n3):
+    if n1 > n2 and n1 > n3:
+        print n1
+    elif n2 > n1 and n2 > n3:
+        print n2
+    elif n3 > n1 and n3 > n2:
+        print n3
+    else:
+        print "Son iguales"
